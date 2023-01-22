72995a54377829bde0705c37a905c4228ea56c42
Wesley Vieira Santos <79053520+swesley400@users.noreply.github.com>
2023-01-22 18:19:53 -0300

    strateggy

--- design-patterns/strateggy/calculadora.ts
+++ design-patterns/strateggy/calculadora.ts
@@ -3,14 +3,13 @@ import Iss from "./iss"
 
 class calculadoraDeImpostos{
     calcular(orcamento, imposto){
-        return imposto.calculaOrcamento(orcamento)
+        return imposto.calculaOrcamento(orcamento);
     }
 }
-
 
 
 const calculoNovo = new calculadoraDeImpostos;
 
 console.log(calculoNovo.calcular(100, new Icms))
-
+console.log(calculoNovo.calcular(100, new Iss))
 
--- design-patterns/strateggy/icms.ts
+++ design-patterns/strateggy/icms.ts
@@ -1,7 +1,7 @@
 import Imposto from "./interfaceImposto";
 
 export default class Icms implements Imposto {
-    calculaOrcamento(orcamento : any){
-        return orcamento * 0.1
+    calculaOrcamento(orcamento : number){
+        return orcamento * 0.1;
     }
 }
